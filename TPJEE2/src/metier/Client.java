73fec9dc56aacb3e5cb496718cc47f790db0987e
legalvincent056 <legalvincent056@gmail.com>
2017-04-10 09:58:07 +0200

    nana

---
+++ TPJEE2/src/metier/Client.java
@@ -0,0 +1,56 @@
+package metier;
+
+public class Client {
+	private int id;
+	private String nom;
+	private String prenom;
+	private String couleuryeux;
+	private int age;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getCouleuryeux() {
+		return couleuryeux;
+	}
+
+	public void setCouleuryeux(String couleuryeux) {
+		this.couleuryeux = couleuryeux;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", couleuryeux=" + couleuryeux + ", age="
+				+ age + "]";
+	}
+
+}
