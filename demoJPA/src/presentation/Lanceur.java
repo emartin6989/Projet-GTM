5c5053158c1c560c69308d59a3d77c88eaf14bfc
emartin6989 <emartin6989@gmail.com>
2017-04-13 15:09:22 +0200

    essai

--- demoJPA/src/presentation/Lanceur.java
+++ demoJPA/src/presentation/Lanceur.java
@@ -1,20 +1,37 @@
 package presentation;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import metier.Contact;
+
 public class Lanceur {
 
 	public static void main(String[] args) {
 		// 1 : ouverture de l'unité de persistance
-
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("demojpa-pu");
+		EntityManager em = emf.createEntityManager();
+		
 		// 2 : ouverture de la transaction
-
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
 		// 3 : création d'un objet métier
-
+		Contact c = new Contact();
+		c.setNom("Christian");
+		c.setPrenom("Eddy");
+		c.setEmail("toto@example.com");
+		
 		// 4 : persistance de l'objet métier
-
+		em.persist(c);
+		
 		// 5: fermeture de la transaction
-
+		tx.commit();
 		// 6 : fermeture de l'unité de persistance
-
+		em.close();
+		emf.close();
 	}
 
 }
