75fe529016dca6fcc8385a2e6500ca1f3f8d08d2
emartin6989 <emartin6989@gmail.com>
2017-04-13 14:57:53 +0200

    youhou

--- demoJPA/src/metier/Contact.java
+++ demoJPA/src/metier/Contact.java
@@ -1,7 +1,14 @@
 package metier;
 
-public class Contact {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Contact {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String nom;
 	private String prenom;
