22321bf25b059b0b00f41723dea28c888116179e
emartin6989 <emartin6989@gmail.com>
2017-04-13 15:02:55 +0200

    hehe

--- demoJPA/src/presentation/Lanceur.java
+++ demoJPA/src/presentation/Lanceur.java
@@ -3,7 +3,17 @@ package presentation;
 public class Lanceur {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// 1 : ouverture de l'unité de persistance
+
+		// 2 : ouverture de la transaction
+
+		// 3 : création d'un objet métier
+
+		// 4 : persistance de l'objet métier
+
+		// 5: fermeture de la transaction
+
+		// 6 : fermeture de l'unité de persistance
 
 	}
 
